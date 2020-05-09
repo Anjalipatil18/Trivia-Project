9e3f9a8435544f2776982d76c4a13e74ea3ef972
Anjalipatil18 <anjalip18@navgurukul.org>
2020-05-09 23:28:52 +0530

    Update knex.js

--- Model/knex.js
+++ Model/knex.js
@@ -1,14 +1,4 @@
-const options = {
-    client: 'mysql',
-    connection: {
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'navgurukul',
-        database: 'Trivia_Project'
-    }
-}
-
-const knex = require('knex')(options);
+const knex=require("./connectionDb")
 
 insertData=(data)=>{
     return knex('Trivia').insert(data)
